1f85fd78a1d6cae3845df673419f633e6f5106bd
3vdi <unitcomics.world@proton.me>
2023-04-02 20:51:26 +0530

    noteview modal

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Typography, Button, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Signup from './Signup';
 
 import image1 from "../images/cloud-log-img-2.png"
 
@@ -12,12 +14,17 @@ const style = {
 
 const Hello = () => {
   return (
-    <div className='container mt-1'>
+    <div className='container'>
     <Box  sx={style}>
       <Box textAlign="center">
         <Typography variant="h3" component="h1" color='grey' gutterBottom>
           Your Notes on Cloud
         </Typography>
+        <Box display='flex' justifyContent='center' alignItems='center' sx={{mb:2}}>
+        <Button variant="contained" color="secondary" component={Link} to='/Signup'>
+          Get Started
+        </Button>
+        </Box>
         <img src={image1} className="img-fluid" width='300px' alt="Cloud-Logs"/>
       </Box>
     </Box>
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useRef } from "react";
 import NoteContext from "../context/notes/NoteContext";
 import { useNavigate } from "react-router-dom";
 
@@ -6,14 +6,46 @@ import { Paper, Box, Typography, IconButton } from "@mui/material";
 import { GrView } from "react-icons/gr";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import { TextField, Container, Button, Modal, Fab, Stack } from '@mui/material';
 
+import { GrClose } from "react-icons/gr";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 1,
+  height: 1,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Noteitem = (props) => {
+
+  const ref = useRef(null)
+    const refClose = useRef(null)
   const context = useContext(NoteContext);
   const navigate = useNavigate();
   const { notes, editNote } = context;
   const { deleteNote } = context;
-  const { note, updateNote } = props;
+  const { note, mynote, updateNote } = props;
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+  const handleModalOpen = (currentNote) => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div className="container justify-content-center">
     <div className='container mx-2 my-2'>
@@ -23,9 +55,91 @@ const Noteitem = (props) => {
           <Box sx={{ '& > :not(style)': { m: 1 }, display: 'flex', p: 1 }}>
           <Typography variant="h6" sx={{ flexGrow: 1}}>
             {note.title}</Typography>
-            <IconButton size="small" color="default" aria-label="view">
+            <IconButton size="small" color="default" aria-label="view" key={note._id} note={note} onClick={handleOpen}>
             <GrView />
           </IconButton>
+
+        
+      <button ref={ref} type="button" onClick={handleOpen} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+    </button>
+            
+    <Modal
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="title"
+    aria-describedby="description"
+>
+    <Box sx={style}>
+
+        <Container maxWidth="lg">
+            <Box sx={{ '& > :not(style)': { m: 1 }, display: 'flex', p: 1 }}>
+                <Typography variant="h3" sx={{ flexGrow: 1, alignItems: 'center'}}>{note.title}</Typography>
+                <Fab size="small" color="default" aria-label="goback" onClick={handleClose}><GrClose /></Fab>
+            </Box>
+            
+        <div className='container mx-3 my-3'>
+      <Box component="form" sx={{ p: 1}}>
+        <TextField
+          fullWidth
+          id="title"
+          name="title"
+          label="Title"
+          variant="standard"
+          size='normal'
+          value={note.title}
+        />
+        <TextField
+          fullWidth
+          id="description"
+          name="description"
+          label="Description"
+          variant="standard"
+          size='small'
+          value={note.description}
+        />
+        <TextField
+          fullWidth
+          id="tag"
+          name="tag"
+          label="Tags"
+          variant="standard"
+          size='small'
+          value={note.tag}
+        />
+      </Box>
+      </div>
+    </Container>
+    </Box>
+    </Modal>
+
+          {/* {modalOpen && (
+        <>
+        <button ref={ref} onClick={handleModalOpen} type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <IconButton size="small" color="default" aria-label="view" key={note._id} onClick={handleModalOpen}>
+            <GrView />
+          </IconButton>
+        </button>
+        
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">{note.title}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+              {note.description}
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </>
+        )} */}
           <IconButton size="small" color="default" aria-label="edit" onClick={()=>{updateNote(note)}}>
             <EditIcon />
           </IconButton>
